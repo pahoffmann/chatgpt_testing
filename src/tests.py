b2ffd7ccf9885491741e3a0f5a5aff1a3d82821d
pahoffmann <pahoffmann@uni-osnabrueck.de>
2023-04-22 01:19:06 +0200

    added initial file, added gitignore

--- src/tests.py
+++ src/tests.py
@@ -1,1 +1,18 @@
-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
+import openai
+
+openai.api_key = "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"
+
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": "Why should DevOps engineer learn kubernetes?"},
+        ]
+)
+
+result = ''
+for choice in response.choices:
+    result += choice.message.content
+
+print(result)
